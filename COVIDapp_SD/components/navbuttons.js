ae3f1daf37d890bd93e8baedd70331ff2d0c53d6
celousan <celousan@bu.edu>
2020-09-14 01:23:28 -0300

    Update navbuttons.js

--- COVIDapp_SD/components/navbuttons.js
+++ COVIDapp_SD/components/navbuttons.js
@@ -55,49 +55,44 @@ import {StyleSheet, TouchableOpacity, Text, View} from 'react-native';
   }
 
 const styles = StyleSheet.create({
-  statusButtonContainer: {
-    backgroundColor: "#ffffff00", //bgc color, can ffffff00 /fff435
-    paddingVertical: 11, //11 for status
-    width: 322, //to cover length of text
-    marginTop: 165 //for status
-    /*remember to check if these values change to other devices*/
-  },
   settingsButtonContainer: {
-    backgroundColor: "#ffffff00", //bgc color, can ffffff00 /fff435
-    paddingVertical: 9, //9 for status
-    paddingHorizontal: 0,
-    width: 55, //to cover length of text
-    marginTop: 8, //for settings
-    left: 350
+    backgroundColor: "#ffd435", //bgc color, can ffffff00 /fff435
+    width: 52, //to cover length of text
+    top: 13, //for settings
+    left: 353
     /*remember to check if these values change to other devices*/
   },
   questionnaireButtonContainer: {
-    backgroundColor: "#ffffff00", //bgc color, can ffffff00 /fff435
-    paddingVertical: 11,
-    paddingHorizontal: 0,
+    backgroundColor: "#ffc435", //bgc color, can ffffff00 /fff435
     width: 210, //to cover length of text
-    height: 60,
-    marginTop: 11,
+    height: 63,
+    top: 0,
     left: 135
+    /*remember to check if these values change to other devices*/
+  },
+  statusButtonContainer: {
+    backgroundColor: "#fff435", //bgc color, can ffffff00 /fff435
+    width: 311, //to cover length of text
+    left: 10,
+    top: 145, //for status
+    height: 50
     /*remember to check if these values change to other devices*/
   },
   facebookButtonContainer: {
     backgroundColor: "#ffffff00", //bgc color, can ffffff00 /fff435
     paddingVertical: 11,
-    paddingHorizontal: 0,
     width: 62, //to cover length of image
     height: 63,
-    marginTop: 622,
+    top: 622,
     left: 176
     /*remember to check if these values change to other devices*/
   },
   googleButtonContainer: {
-    backgroundColor: "#fff435", //bgc color, can ffffff00 /fff435
+    backgroundColor: "#ffffff00", //bgc color, can ffffff00 /fff435
     paddingVertical: 11,
-    paddingHorizontal: 0,
     width: 62, //to cover length of image
     height: 63,
-    marginTop: 665,
+    top: 640,
     left: 176
     /*remember to check if these values change to other devices*/
   },
