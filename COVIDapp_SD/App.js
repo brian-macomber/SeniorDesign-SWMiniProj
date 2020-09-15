3e4fd4f6402600e2f66033182ec154c108a05f70
celousan <celousan@bu.edu>
2020-09-15 02:18:10 -0300

    Separare Page file for HomePage

--- COVIDapp_SD/App.js
+++ COVIDapp_SD/App.js
@@ -1,5 +1,5 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -25,6 +25,7 @@ import Questionnaire from './screens/Questionnaire';
 import CompletedQuestionnaire from './screens/CompletedQuestionnaire';
 import Settings from './screens/Settings';
 import Status from './screens/Status';
+import HomePage from './screens/HomePage';
 
 const Stack = createStackNavigator();
 
@@ -43,29 +44,6 @@ const LoginScreen = ({navigation}) => {
             />
       </ImageBackground>
     </View>
-  );
-};
-
-const HomePage = ({navigation}) => {
-  return (
-  <View style={styles.container}>
-    <ImageBackground source={require('backgrounds/appiphonehomeqg.png')} resizeMode='stretch' style={styles.imgBackg}>
-      <Text></Text>
-      {/* Navigation is working between all pages -> Now need to check screen portability */}
-      <SettingsButton
-        title="Settings"
-        onPress={() => navigation.navigate('Settings')}
-      />
-      <QuestionnaireButton
-        title="Questionnaire"
-        onPress={() => navigation.navigate('Questionnaire')}
-      />
-      <StatusButton
-        title="Status"
-        onPress={() => navigation.navigate('Status')}
-      />
-    </ImageBackground>
-  </View>
   );
 };
 
