15a16ee655a63587ab2d74c6c8fcebabcb5fb456
celousan <celousan@bu.edu>
2020-09-15 02:50:25 -0300

    Update HomePage.js

--- COVIDapp_SD/screens/HomePage.js
+++ COVIDapp_SD/screens/HomePage.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, ImageBackground, TouchableOpacity} from 'react-native';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, ImageBackground, TouchableOpacity, FlatList} from 'react-native';
 import {FacebookButton, GoogleButton, StatusButton, SettingsButton, QuestionnaireButton} from '../components/navbuttons';
 
 import {NavigationContainer} from '@react-navigation/native';
@@ -8,27 +8,43 @@ import {NavigationContainer} from '@react-navigation/native';
 class HomePage extends Component{
     constructor(props) {
       super(props)
+      this.state = {
+        items: [{title: 'Test 1'}],
+        isLoading: false
+      }
+    }
+
+    renderRow = ({item, index}) => {
+      return(
+        <View style = {{flex: 1, height: '100%', width: '100%'}}>
+        <ImageBackground source={require('backgrounds/appiphonehomeqg.png')} resizeMode='stretch' style={styles.imgBackg}>
+         <Text>{item.title}</Text>
+         <SettingsButton
+           title="Settings"
+           onPress={() => this.props.navigation.navigate('Settings')}
+         />
+         <QuestionnaireButton
+           title="Questionnaire"
+           onPress={() => this.props.navigation.navigate('Questionnaire')}
+         />
+         <StatusButton
+           title="Status"
+           onPress={() => this.props.navigation.navigate('Status')}
+         />
+         </ImageBackground>
+        </View>
+      )
     }
 
     render() {
         return (
           <View style={styles.container}>
-            <ImageBackground source={require('backgrounds/appiphonehomeqg.png')} resizeMode='stretch' style={styles.imgBackg}>
-              <Text></Text>
               {/* Navigation is working between all pages -> Now need to check screen portability */}
-              <SettingsButton
-                title="Settings"
-                onPress={() => this.props.navigation.navigate('Settings')}
-              />
-              <QuestionnaireButton
-                title="Questionnaire"
-                onPress={() => this.props.navigation.navigate('Questionnaire')}
-              />
-              <StatusButton
-                title="Status"
-                onPress={() => this.props.navigation.navigate('Status')}
-              />
-            </ImageBackground>
+              <FlatList
+                data = {this.state.items}
+                renderItem = {this.renderRow}
+                keyExtractor = {(i, k) => k.toString()}
+                />
           </View>
         );
     }
