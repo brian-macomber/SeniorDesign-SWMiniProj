8195f5c94d915b5b5098eb1ce5912c7f060d4ea5
celousan <celousan@bu.edu>
2020-09-15 02:53:52 -0300

    RollBack

---
+++ COVIDapp_SD/screens/HomePage.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, ImageBackground, TouchableOpacity} from 'react-native';
+import {FacebookButton, GoogleButton, StatusButton, SettingsButton, QuestionnaireButton} from '../components/navbuttons';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+
+class HomePage extends Component{
+    constructor(props) {
+      super(props)
+    }
+
+    render() {
+        return (
+          <View style={styles.container}>
+            <ImageBackground source={require('backgrounds/appiphonehomeqg.png')} resizeMode='stretch' style={styles.imgBackg}>
+              <Text></Text>
+              {/* Navigation is working between all pages -> Now need to check screen portability */}
+              <SettingsButton
+                title="Settings"
+                onPress={() => this.props.navigation.navigate('Settings')}
+              />
+              <QuestionnaireButton
+                title="Questionnaire"
+                onPress={() => this.props.navigation.navigate('Questionnaire')}
+              />
+              <StatusButton
+                title="Status"
+                onPress={() => this.props.navigation.navigate('Status')}
+              />
+            </ImageBackground>
+          </View>
+        );
+    }
+}
+
+export default HomePage;
+
+
+const styles = StyleSheet.create({
+    container: {
+        minWidth: '100%', //70
+        maxWidth: '100%', //90
+        alignItems: 'stretch',
+        justifyContent: 'center',
+        elevation: 20,
+        borderRadius: 10,
+        flex: 1,
+    },
+    selectionGroupContainer: {
+        flexDirection: 'column',
+        backgroundColor: 'white',
+        alignContent: 'flex-end',
+    },
+    imgBackg:{
+      width: '100%',
+      height: '100%',
+      flex: 1
+    }
+});
