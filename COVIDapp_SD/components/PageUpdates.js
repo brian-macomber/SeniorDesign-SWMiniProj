3ec201603ff836e2b859e3c7a259ca3626feef8c
celousan <celousan@bu.edu>
2020-09-17 16:35:29 -0300

    Updates to BG Logic

--- COVIDapp_SD/components/PageUpdates.js
+++ COVIDapp_SD/components/PageUpdates.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
@@ -6,41 +6,31 @@ import {
   ImageBackground,
 } from 'react-native';
 
-class PageUpdates{
-  constructor(props){
-    super(props);
-  }
 
-
-  statusPageUpdate(badgeId){
-    if(badgeId == 'Green'){
+  export function statusPageUpdate({badgeParam}){
+    if(badgeParam == 'Green'){
       const imageStatus = require('../backgroundfiles/appiphonebadgeg.png');
-    }else if(badgeId == 'Yellow'){
+    }else if(badgeParam == 'Yellow'){
       const imageStatus = require('../backgroundfiles/appiphonebadgey.png');
-    }else if(badgeId == 'Red'){
+    }else if(badgeParam == 'Red'){
       const imageStatus = require('../backgroundfiles/appiphonebadger.png');
     }
-    return(
-      <View style={styles.container}>
-        <ImageBackground source={imageStatus} resizeMode='stretch' style={styles.imgBackg}>
-          <Text></Text>
-        </ImageBackground>
-      </View>
-    );
   }
-  
-}
-
-export default PageUpdates;
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: Colors.lighter,
-    flex: 1,
+      minWidth: '100%', //70
+      maxWidth: '100%', //90
+      alignItems: 'stretch',
+      justifyContent: 'center',
+      elevation: 20,
+      borderRadius: 10,
+      flex: 1,
   },
   imgBackg:{
     width: '100%',
     height: '100%',
-    flex: 1
+    flex: 1,
+    resizeMode: 'stretch'
   }
 });
--- COVIDapp_SD/screens/Status.js
+++ COVIDapp_SD/screens/Status.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {SafeAreaView, StyleSheet, ScrollView, View, Text, StatusBar, Button, ImageBackground, TouchableOpacity} from 'react-native';
 import {FacebookButton, GoogleButton, StatusButton, SettingsButton, QuestionnaireButton} from '../components/navbuttons';
+import {statusPageUpdate} from '../components/PageUpdates';
 
 export default class Status extends Component {
     constructor(props) {
@@ -11,10 +12,13 @@ export default class Status extends Component {
         const {badgeId} = this.props.route.params
         console.log('passed badge id to status is:');
         console.log(badgeId);
+        const bgFile = statusPageUpdate({badgeId});
         return (
           //update background layout/contructor color - done
                 <View style={styles.container}>
-                  <ImageBackground source={require('backgrounds/appiphonebadgeg.png')} resizeMode='stretch' style={styles.imgBackg}>
+                  <ImageBackground source={require('backgrounds/appiphonehomeqg.png')}
+                  resizeMode="stretch"
+                  style={styles.imgBackg}>>
                   <Text></Text>
                   <Button
                     style = {{alignSelf: 'center'}}
@@ -47,6 +51,7 @@ const styles = StyleSheet.create({
     imgBackg:{
       width: '100%',
       height: '100%',
-      flex: 1
+      flex: 1,
+      resizeMode: 'stretch'
     }
 });
